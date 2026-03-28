dfddb35d590545e9ac9ba2bceda97e6aef08ca1c
Abhay <abhay.work2026@gmail.com>
2026-03-28 10:55:52 +0530

    acad

---
+++ variables.cpp
@@ -0,0 +1,19 @@
+//operating with variables
+#include <iostream
+using namespace std;
+int main ()
+{
+    // declaring variables
+    int a, b;
+    int result;
+    
+    // Process
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    //print out the result
+    cout << result << endl;
+    return 0;
+}
---
+++ veriables_v2.cpp
@@ -0,0 +1,23 @@
+//operating with variables
+#include <iostream>
+using namespace std;
+int main ()
+{
+    // declaring variables
+    int a, b;
+    int result;
+    
+    // Process
+    cout << "SUM of two numbers" << endl;
+    cout << "enter a value for a: ";
+    cin >> a;
+    cout << "enter a value for b: ";
+    cin >> b;
+    a = a + 1;
+    cout << "increasing a by 1" << endl;
+    result = a - b;
+
+    //print out the result
+    cout << "result is:  " << result <<  endl;
+    return 0;
+}
