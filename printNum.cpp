e85fe55eda0b643781ca5cb4b9f2a97a514909f5
Abhay <abhay.work2026@gmail.com>
2026-03-29 11:49:40 +0530

    acad

---
+++ printNum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int num1, num2;
+    
+    
+    cout << "enter two numbers: ";
+    cin >> num2 >> num1;
+    cout << "you entered: " << num1 << " and " << num2 << endl;
+    return 0;
+}
---
+++ printNum_v2.cpp
@@ -0,0 +1,13 @@
+// this is improved version of printNum.cpp
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "enter the first number:";
+    cin >> num1;
+    cout << "enter the second number:";
+    cin >> num2;
+    cout << "you entered: " << num1 << " and " << num2 << endl;
+    return 0;}
