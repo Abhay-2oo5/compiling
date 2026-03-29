2f3a4f9486d20491797541196878b6b8fc0adc7c
Abhay <abhay.work2026@gmail.com>
2026-03-29 09:04:28 +0530

    acad

---
+++ myString.cpp
@@ -0,0 +1,14 @@
+//my first string 
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string myString = "Hello World!";
+    myString = "This is a initial string content.";
+    cout << myString <<endl;
+    myString  = "This is a new string content.";
+    cout << myString <<endl;
+    return 0;
+    
+}
